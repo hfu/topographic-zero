e1baf079ecc8a592e0d55befae54584f8ab3d246
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-01-06 17:26:33 -0500

    update

--- index.js
+++ index.js
@@ -6,36 +6,50 @@ const path = require('path')
 
 const svgs = glob.sync(config.get('src'))
   .map(p => {
-console.log(p)
     return {
       svg: fs.readFileSync(p),
       id: path.basename(p).replace('.svg', '')
     }
   })
 
-spritezero.generateLayout(
-  {
-    imgs: svgs,
-    pixelRatio: config.get('ratio'),
-    format: true
-  },
-  (err, dataLayout) => {
-    if (err) throw err
-    fs.writeFileSync('sprite.json', JSON.stringify(dataLayout))
-  }
-)
+const postfix = m => {
+  switch (m) {
+    case 1:
+      return ''
+    default:
+      return `@${m}x`
+ }
+}
 
-spritezero.generateLayout(
-  {
-    imgs: svgs,
-    pixelRatio: config.get('ratio'),
-    format: false
-  },
-  (err, imageLayout) => {
-    if (err) throw err
-    spritezero.generateImage(imageLayout, (err, image) => {
+for (const m of [1, 2, 3]) {
+  spritezero.generateLayout(
+    {
+      imgs: svgs,
+      pixelRatio: config.get('ratio') * m,
+      format: true
+    },
+    (err, dataLayout) => {
       if (err) throw err
-      fs.writeFileSync('sprite.png', image)
-    })
-  }
-)
+      fs.writeFileSync(
+        `sprite${postfix(m)}.json`, 
+        JSON.stringify(dataLayout)
+      )
+    }
+  )
+
+  spritezero.generateLayout(
+    {
+      imgs: svgs,
+      pixelRatio: config.get('ratio') * m,
+      format: false
+    },
+    (err, imageLayout) => {
+      if (err) throw err
+      spritezero.generateImage(imageLayout, (err, image) => {
+        if (err) throw err
+        fs.writeFileSync(`sprite${postfix(m)}.png`, image)
+      })
+    }
+  )
+}
+
